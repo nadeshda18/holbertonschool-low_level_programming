b09243688eceae133bdb09a8832a70d56c9c205c
nadeshda18 <nadeshda02@hotmail.com>
2023-06-15 23:47:11 +0000

    printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - print exactly a line with printf function
  *
- * Return: (0) (Success)
+ * Return: 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,"\n);
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
