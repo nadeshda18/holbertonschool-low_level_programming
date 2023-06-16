90b67128a6a4ef611caa837d47be66558c85b639
nadeshda18 <nadeshda02@hotmail.com>
2023-06-16 00:22:40 +0000

    various types

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,10 +12,10 @@ long int li;
 long long int lli;
 float f;
 
-printf("Size of a char: %y byte(s)\n", sizeof(c));
-printf("Size of an int: %y byte(s)\n", sizeof(i));
-printf("Size of a long int: %y byte(s)\n" sizeof(li));
-printf("Size of a long long int: %y (byte)s\n", sizeof(lli));
-printf("Size of a float: %y byte(s)\n", sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+printf("Size of a long int: %lu byte(s)\n" (unsigned long)sizeof(li));
+printf("Size of a long long int: %lu (byte)s\n", (unsigned long)sizeof(lli));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
