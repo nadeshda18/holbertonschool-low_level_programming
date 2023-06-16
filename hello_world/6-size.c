cca0274ebeadf3ab5d9c4b3236769afcb1813035
nadeshda18 <nadeshda02@hotmail.com>
2023-06-16 00:12:30 +0000

    types on the computer

---
+++ hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the size of various types it is compiled and run on
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char c;
+int i;
+long int li;
+long long int lli;
+float f;
+
+printf("Size of a char: %y byte(s)\n", size of (c));
+printf("Size of an int: %y byte(s)\n", size of (i));
+printf("Size of a long int: %y byte(s)\n" size of (li));
+printf("Size of a long long int: %y (byte)s\n" size of (lli));
+printf("Size of a float: %y byte(s)\n" size of (f));
+return (0);
+}
