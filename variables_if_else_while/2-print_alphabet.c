b2b016514200bad8c83bbef8c8f8f818054a3ca7
nadeshda18 <nadeshda02@hotmail.com>
2023-06-19 22:01:10 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - print the alphabet in lower case
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+char ch = 'a';
+
+while (ch = 'a', ch <= 'z', ch++)
+{
+putchar(ch);
+putchar('\n');
+}
+return (0);
+}
