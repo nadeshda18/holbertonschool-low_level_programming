e6ca30a8964b25ab509c418e0c093852dfbf338c
nadeshda18 <nadeshda02@hotmail.com>
2023-06-20 15:20:59 +0000

    Hexa

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - hexadecimal print numbers of base 16
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+int num;
+char cha;
+
+for (num = 0; num <= 10; num++);
+{
+putchar(num + '0');
+}
+for (cha = 'a'; cha <= 'g'; cha++);
+{
+putchar(cha);
+}
+putchar('\n');
+return (0);
+}
