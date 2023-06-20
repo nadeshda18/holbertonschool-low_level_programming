cce9a3bdf0b4322156c2360808efa2f5f452dd79
nadeshda18 <nadeshda02@hotmail.com>
2023-06-20 17:10:57 +0000

    combo

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main - print all possible combinations of single-digit numbers
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+int x;
+
+for (x = 0 ; x <= 9 ; x++)
+{
+putchar(x + '0');
+}
+{
+putchar(',');
+}
+return (0);
+}
