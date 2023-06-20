1f80bc95651652377d4e2689cef310ca0568b295
nadeshda18 <nadeshda02@hotmail.com>
2023-06-20 03:09:52 +0000

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print all numbers from base 10
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
