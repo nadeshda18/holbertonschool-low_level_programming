c2d3eee4c46a4bef3ed3247d7d1eabda2e12045a
nadeshda18 <nadeshda02@hotmail.com>
2023-06-20 03:03:53 +0000

    numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - print all single digit numbers of base 10
+*
+* Return: Always (0)
+*/
+int main(void)
+{
+int x;
+for (x = 0 ; x < 10; x++)
+{
+printf("%d", x);
+}
+printf("\n");
+return (0);
+}
