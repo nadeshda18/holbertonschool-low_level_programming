c3418fe7a5791e3ea44c4fe06515e439d518ad35
nadeshda18 <nadeshda02@hotmail.com>
2023-06-21 16:45:24 +0000

    is lower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ * int _islower : checks for lowercase letters
+ *
+ * Return: (0)
+ */
+int _islower(int c);
+
+int main(void)
+{
+int r = 65;
+int o = 97;
+
+if (r > 64 && r < 91)
+{
+_putchar(r + '0');
+}
+
+if (o > 96 && o < 123)
+{
+_putchar(o + '0');
+}
+	while (r == 99)
+	return (1);
+
+_putchar('\n');
+return (0);
+}
