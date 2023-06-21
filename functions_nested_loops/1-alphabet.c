ab4dfa664a0c8104a8a6e8975796d27bd0e0ed2a
nadeshda18 <nadeshda02@hotmail.com>
2023-06-21 01:14:18 +0000

    alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - write the alphabet in lowercase
+ *
+ */
+void print_alphabet(void)
+{
+
+char ch = 'a';
+
+while (ch <= 'z')
+{
+	_putchar(ch);
+	ch++;
+}
+_putchar('\n');
+}
