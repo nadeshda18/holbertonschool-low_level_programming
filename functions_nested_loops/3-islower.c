35240de85803a5eabadeebaf39e119f7105979f3
nadeshda18 <nadeshda02@hotmail.com>
2023-06-21 18:07:43 +0000

    lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,7 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _islower: verify all lower case letters
+ * _islower - verify lower case letters
+ *
+ * @c: returns the value of one or zero
  *
  * Return: Always 0 (Success)
  */
