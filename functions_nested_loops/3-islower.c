fb7192f7286d1c4a4d41090b352f47acc1176544
nadeshda18 <nadeshda02@hotmail.com>
2023-06-21 17:44:15 +0000

    lower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -8,10 +8,12 @@
  */
 int _islower(int c)
 {
-if (c >= 64 && c <= 122)
-{
-	while (c == 99)
-	return (1);
-}
-return (0);
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
