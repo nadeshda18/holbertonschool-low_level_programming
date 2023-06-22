800a7701f8bfdd93e9438e67f03f49e4dabd14cf
nadeshda18 <nadeshda02@hotmail.com>
2023-06-22 02:32:30 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *int add - add two numbers and return the result
+ *@a: first number
+ *@b: second number
+ *
+ * Return: Sum of two numbers
+ */
+int add(int a, int b)
+{
+int sum = (a + b);
+
+return (sum);
+}
