18aea3c90a51af6c5569e04490add54010c7851b
nadeshda18 <nadeshda02@hotmail.com>
2023-06-22 02:51:39 +0000

    98 numbers

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - starting from 0, print all numbers
+ * @n: the number to be printed
+ *
+ * Return: (0)
+ */
+void print_to_98(int n)
+{
+for (n = 0 ; n <= 99 ; n++)
+printf("%i", n);
+
+}
