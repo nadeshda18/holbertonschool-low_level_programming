1497ed789bb8db8cf9d604661b6e78d118c7b9fa
nadeshda18 <nadeshda02@hotmail.com>
2023-06-22 14:03:37 +0000

    Jack Bauer

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,11 +8,11 @@ void jack_bauer(void)
 {
 int a, b, c, d;
 
-for (a = 0; a <= 3; a++)
+for (a = 0; a <= 2; a++)
 {
-for (b = 0; b <= 3; b++)
+for (b = 0; b <= 9; b++)
 {
-if ((a <= 2 && b <= 3) || (c <= 5 && d <= 9))
+if ((a <= 1 && b <= 9) || (a <= 2 && b <= 3))
 {
 for (c = 0; c <= 5; c++)
 {
