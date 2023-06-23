d149c21eb20aeecb2c3a2e01c316321924ebb921
nadeshda18 <nadeshda02@hotmail.com>
2023-06-23 22:24:25 +0000

    most numbers

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -3,23 +3,17 @@
 /**
  * print_most_numbers - all but two and four
  *
- * @x: value for most numbers
- *
  * Return: (0)
  */
 void print_most_numbers(void)
 {
 	int x;
-
-	for (x = 0; x <= 9; x++)
 	{
+	for (x = 0; x <= 9; x++)
+
 		if (x != 2 && x != 4)
-		break;
-		
-	}
 	{
 	_putchar(x + '0');
-	_putchar('\n');
+	}
 	}
 }
-
