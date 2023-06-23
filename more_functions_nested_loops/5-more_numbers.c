268b8e1f0b65b29e6adb78d1a9cc21330d9e03a3
nadeshda18 <nadeshda02@hotmail.com>
2023-06-23 23:40:17 +0000

    more numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,12 @@ void more_numbers(void)
 	for (x = 0; x < 10; x++)
 	{
 		for (j = 0; j <= 14; j++)
-			{
+		{
 			if (j >= 10)
-				_putchar((j / 10) + '0');
+					_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
-			}
+		}
+
 		_putchar('\n');
 	}
 }
