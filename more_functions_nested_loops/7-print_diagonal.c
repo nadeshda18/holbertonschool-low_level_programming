c7f8847636aef6bcb7fba92bc58aef2d8c2acb54
nadeshda18 <nadeshda02@hotmail.com>
2023-06-24 01:02:00 +0000

    diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -8,16 +8,21 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n > 0)
 	{
 		for (i = 0; i < n; i++)
 		{
-		_putchar(92);
+			for (j = 0; j < n; j++)
+			{
+			_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
 		}
-
 	}
+	else
 	_putchar('\n');
 }
 
