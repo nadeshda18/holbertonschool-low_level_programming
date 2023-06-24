4e0706d5885f08c403af208f1f861d451cc90392
nadeshda18 <nadeshda02@hotmail.com>
2023-06-24 00:10:31 +0000

    lines

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -3,8 +3,7 @@
 /**
  * print_line - draw straight lines
  *
- * @n: finish with character n
- * @i: as many times
+ * @n: finish with character
  *
  * Return: void
  */
@@ -12,7 +11,7 @@ void print_line(int n)
 {
 	int i;
 
-	if (n <= 0)
+	if (n > 0)
 	{
 			for (i = 0; i <= n; i++)
 				{
