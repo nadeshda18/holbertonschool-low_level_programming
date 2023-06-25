ecd3b77a6a50cccad23216a0f36bc1d40f800332
nadeshda18 <nadeshda02@hotmail.com>
2023-06-25 17:57:26 +0000

    print combo

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,16 +8,16 @@ int main(void)
 {
 	int i;
 	int j;
-	
+
 	for (i = 0; i <= 9; i++)
 	{
 	for (j = i +1; j <= 9; j++)
 	{
 	putchar(i + '0');
-	putchar(j +'0');
+	putchar(j + '0');
 	putchar(',');
 	putchar(' ');
 	}
 	}
-	return 0;
+	return (0);
 }
