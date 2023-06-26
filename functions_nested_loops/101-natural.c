ec23a1d71682a3c1d201819352d2026605dfc91f
nadeshda18 <nadeshda02@hotmail.com>
2023-06-26 02:22:18 +0000

    natural

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print multiples of numbers three and five
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int x = 3;
+
+	for (x = 3; x = (x * 2); x = (x * 3))
+	{
+	printf("%d\n", x);
+	}
+}
