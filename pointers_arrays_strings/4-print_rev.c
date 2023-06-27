109eeaf764c15146139bcb055943a07daeba8392
nadeshda18 <nadeshda02@hotmail.com>
2023-06-27 03:04:36 +0000

    reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - print a verse in reverse
+ * @s: character name
+ *
+ */
+void print_rev(char *s)
+{
+	char a = 0;
+	char b;
+
+	while (*s != '\0')
+	{
+	a++;
+	s++;
+	}
+	s--;
+	for (b = a; b > 0; b--)
+	{
+	_putchar(*s);
+	s--;
+	}
+	_putchar('\n');
+}
