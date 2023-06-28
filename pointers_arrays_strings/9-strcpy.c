2611daa0a092178938b7851c5245ec7fbb47fa01
nadeshda18 <nadeshda02@hotmail.com>
2023-06-28 17:22:24 +0000

    copy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * *_strcpy - copy
+ * @dest: buffer
+ * @src: pointer
+ *
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for (; i < src[i]; i++)
+	{
+	dest[i] = '\0';
+	dest[i]++;
+	}
+	return (dest);
+}
+
