61abf734182006ca39e65573e2cc1bf1ddbbc523
nadeshda18 <nadeshda02@hotmail.com>
2023-06-28 00:36:13 +0000

    print every other number

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts2 - print every other character from a string, starting with the first letter
+ * @str: pointer
+ *
+ * Return: (0)
+ */
+void puts2(char *str)
+{
+	int a = 0;
+	char *b = str;
+	int m = 0;
+	int n;
+	
+	while (*b != '\0')
+	{
+	b++;
+	a++;
+	}
+	m = a--;
+	for (n = 0; n <= m; n++)
+	{
+	if (n % 2 == 0)
+	{
+	_putchar(str[n]);
+	}
+	}
+	_putchar('\n');
+}
