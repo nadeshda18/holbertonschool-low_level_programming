c6f47b343ef90ee51af5d37beccaf012282f47c0
nadeshda18 <nadeshda02@hotmail.com>
2023-06-28 22:26:25 +0000

    append

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - combine two strings
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+	i++;
+	j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
