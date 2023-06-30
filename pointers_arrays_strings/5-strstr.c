5cccaa3efe4191ffa881b9dddecdc892a91bb60c
nadeshda18 <nadeshda02@hotmail.com>
2023-06-30 20:49:56 +0000

    locate

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -15,8 +15,8 @@ char *_strstr(char *haystack, char *needle)
 	{
 		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (haystack[i] == needle[j])
-				return (haystack);
+			if (needle[j] == haystack[i])
+				return (needle);
 			i++;
 		}
 	}
