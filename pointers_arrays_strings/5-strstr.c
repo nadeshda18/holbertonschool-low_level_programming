e2357eb5a803403061ea73de08fade213e8882b5
nadeshda18 <nadeshda02@hotmail.com>
2023-06-30 00:46:41 +0000

    needle

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strstr - locates a substring
+ * @haystack: to be searched
+ * @needle: substring
+ *
+ * Return: needle or NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+				return (needle);
+		}
+	}
+	return (NULL);
+}
