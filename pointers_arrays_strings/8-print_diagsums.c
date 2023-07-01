3dd9c9252ea0f8a44330410951477aeb5c321da7
nadeshda18 <nadeshda02@hotmail.com>
2023-07-01 02:12:38 +0000

    diagonal

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,22 +7,21 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, j, princ, sec;
+	int i = 0;
+	int j = size -1;
+	int sum1 = 0;
+	int sum2 =0;
 
-	for (i = 0; i < size; i++)
+	while (i <= (size * size))
 	{
-		for (j = 0; j < size; j++)
-		{
-			if (i == j)
-			{
-				princ = a[i] + a[j];
-			}
-
-			if ((i + j) == (size - 1))
-			{
-				sec = a[i] + a[j];
-			}
-		}
+		sum1 = sum1 + a[i];
+		i = i + size + 1;
 	}
-	printf("%d, %d\n", princ, sec);
+
+	while (j < (size * size - 1))
+	{
+		sum2 += a[j];
+		j = j + size - 1;
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
