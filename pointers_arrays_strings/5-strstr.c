e80f1c4466d3e6f3b69c6f9200bf83c0060110ee
nadeshda18 <nadeshda02@hotmail.com>
2023-07-01 02:24:02 +0000

    needle

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -9,20 +9,23 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i;
+	int j = 0;
 
-	for (i = 0; haystack[i] != '\0'; i++)
+	while (needle[j] != '\0')
+		j++;
+
+	while (*haystack)
 	{
-		for (j = 0; needle[j] != '\0'; j++)
+		for (i = 0; needle[i]; i++)
 		{
-			while (haystack[i] == needle[j] && needle[j] != '\0')
-			{
-				j++;
-			}
-
-			if (needle[j] == '\0')
-				return (haystack);
+			if (haystack[i] != needle[i])
+				break;
 		}
+		if (i != j)
+			haystack++;
+		else
+			return (haystack);
 	}
 	return (NULL);
 }
