515177ae173ce436eec11c9732bc9f9cf8d03d9c
nadeshda18 <nadeshda02@hotmail.com>
2023-07-05 18:37:02 +0000

    whats my name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ *
+ *
+ *
+ *
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+
+		printf("%s\n", argv[i]);
+
+	return (0);
+}
