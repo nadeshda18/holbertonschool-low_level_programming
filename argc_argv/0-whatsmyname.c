a8760e159ec78fc82851f803b62f38c69725dbc9
nadeshda18 <nadeshda02@hotmail.com>
2023-07-05 18:42:59 +0000

    whats my name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * main - entry point
+ *@argc: count
+ *@argv: arguments
  *
- *
- *
- *
+ *Return: 0
  */
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 
 		printf("%s\n", argv[i]);
