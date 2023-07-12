aca3df69a080bb340388278b463e07703df1bc06
nadeshda18 <nadeshda02@hotmail.com>
2023-07-12 19:12:10 +0000

    check malloc

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - check if malloc failed
+ * @b: unsigned integer
+ *
+ * Return: (98) if failed
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr;
+
+	ptr = malloc(sizeof(b));
+	{
+	if (ptr == NULL)
+		exit(98);
+	}
+	return (ptr);
+}
