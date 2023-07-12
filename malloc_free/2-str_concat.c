bfd08a53566a49b18a0ebfcd9e97f5b8e650279f
nadeshda18 <nadeshda02@hotmail.com>
2023-07-12 03:40:12 +0000

    concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	int i, j;
 	char *ptr = malloc(sizeof(char) * strlen(s1) * strlen(s2) + 1);
 
-	if (s1 == NULL || s2 == NULL)
+	if (ptr == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
@@ -25,5 +25,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		ptr[i++] = s2[j];
 	}
+	if (!ptr)
+		return (NULL);
+
 	return (ptr);
 }
