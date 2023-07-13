ab15d94b69f5716afb50371a4852520458bad7e0
nadeshda18 <nadeshda02@hotmail.com>
2023-07-13 03:06:08 +0000

    concat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -21,15 +21,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
+	if (n >= strlen(s2))
+
 	ptr = malloc(strlen(s1) + strlen(s2) + 1);
+
+	else
+
+		ptr = malloc(strlen(s1) + n + 1);
 
 	if (ptr == NULL)
 		return (NULL);
-
-	if (n >= strlen(s2))
-	{
-		n = strlen(s2);
-	}
 
 	for (i = 0; s1[i] != '\0'; i++)
 	{
@@ -38,7 +39,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	for (j = 0; s2[j] != '\0'; j++)
 	{
-		ptr[i++] = s2[j] + n;
+		ptr[i++] = s2[j];
 	}
 	return (ptr);
 }
