3d599e11a6ff5a77dfbe8c45ca55c4798397c604
nadeshda18 <nadeshda02@hotmail.com>
2023-07-19 03:24:12 +0000

    print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,9 +9,9 @@
 void print_dog(struct dog *d)
 {
 	if (d->name == NULL)
-		printf("Name: (nil)");
+		d->name = "(nil)";
 	if (d->owner == NULL)
-		printf("(nil)");
+		d->owner = "(nil)";
 
 	if (d == NULL)
 		return;
