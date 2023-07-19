4045455d4c870d6ad90fe73f6ed54836c060174f
nadeshda18 <nadeshda02@hotmail.com>
2023-07-19 05:28:51 +0000

    free the dogs

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - free the allocated memory
+ * @d: pointer
+ *
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+	free(d->name);
+	free(d->owner);
+	free(d);
+	}
+}
