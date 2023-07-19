d549458681d34ee9ebba1fa866f1583a3e610794
nadeshda18 <nadeshda02@hotmail.com>
2023-07-19 00:55:28 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - new type element
+ * @name: char
+ * @age: float
+ * @owner: char
+ */
 
 struct dog
 {
-  char *name;
-  float age;
-  char *owner;
+char *name;
+float age;
+char *owner;
 };
 
 #endif
