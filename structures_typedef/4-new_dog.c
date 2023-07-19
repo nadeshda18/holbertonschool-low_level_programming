355e8c73adc748bac07dac6d191d3fcf88c320d8
nadeshda18 <nadeshda02@hotmail.com>
2023-07-19 05:20:55 +0000

    new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -30,12 +30,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = malloc(strlen(owner) + 1);
 	if (new_dog->owner == NULL)
 	{
+		free(new_dog->name);
 		free(new_dog);
 		return (NULL);
 	}
-
 	strcpy(new_dog->owner, owner);
 
 	new_dog->age = age;
+
 	return (new_dog);
 }
