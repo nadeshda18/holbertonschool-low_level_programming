30d126eaa6df5ae3ad09ea99124378d8602e14e5
nadeshda18 <nadeshda02@hotmail.com>
2023-08-04 15:04:15 +0000

    add at end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -15,7 +15,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *temp = *head;
 
 	new = malloc(sizeof(dlistint_t));
-	if (!new)
+	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
