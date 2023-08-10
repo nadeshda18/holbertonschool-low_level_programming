2c50269a684d1e858e7b6b8b0276bd3aa5766921
nadeshda18 <nadeshda02@hotmail.com>
2023-08-10 05:03:47 +0000

    create a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -22,14 +22,12 @@ int create_file(const char *filename, char *text_content)
 
 	for (i = 0; text_content[i] != '\0'; i++)
 
-	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
-	write(new_file, text_content, i);
+		new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (new_file == -1)
 		return (-1);
 
-	close(new_file);
+	write(new_file, text_content, i);
 
 	return (1);
 }
