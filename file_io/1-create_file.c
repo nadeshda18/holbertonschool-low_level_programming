5209924ef70eeedc51b32932ee80165286a60264
nadeshda18 <nadeshda02@hotmail.com>
2023-08-10 01:29:37 +0000

    write a function that creates a file

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -24,10 +24,10 @@ int create_file(const char *filename, char *text_content)
 
 	new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
+	write(new_file, text_content, i);
+
 	if (new_file == -1)
 		return (-1);
-
-	write(new_file, text_content, i);
 
 	close(new_file);
 
