3586cc6ea643d4e364d5d8228e98df78e2cf997c
nadeshda18 <nadeshda02@hotmail.com>
2023-08-11 14:55:05 +0000

    SEMICOLLON;

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 
-	for (i = 0; text_content[i] != '\0'; i++)
+	for (i = 0; text_content[i] != '\0'; i++);
 
 		new_file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
