92527176ff534a934371ae46625395193912d49c
nadeshda18 <nadeshda02@hotmail.com>
2023-08-11 19:36:25 +0000

    bug fixes

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,6 +1,6 @@
+#include "hash_tables.h"
 #include <stdlib.h>
 #include <string.h>
-#include "hash_tables.h"
 /**
  * hash_table_set - Adds an element to the hash table
  * @ht: Pointer to the hash table
@@ -12,24 +12,33 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *current_node;
 
 	if (ht == NULL || key == NULL || value == NULL || strlen(key) == 0)
 		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
-
+	for (current_node = ht->array[index]; current_node != NULL;
+			current_node = current_node->next)
+	{
+		if (strcmp(current_node->key, key) == 0)
+		{
+			free(current_node->value);
+			current_node->value = strdup(value);
+			if (current_node->value == NULL)
+				return (0);
+			return (1);
+		}
+	}
 	new_node = malloc(sizeof(hash_node_t));
+	free(new_node->value);
 	if (new_node == NULL)
 		return (0);
-
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
 		free(new_node);
 		return (0);
 	}
-
 	new_node->value = strdup(value);
 	if (new_node->value == NULL)
 	{
@@ -37,9 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new_node);
 		return (0);
 	}
-
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
-
 	return (1);
 }
